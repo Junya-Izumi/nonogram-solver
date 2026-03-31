a0e26f158914b3d6d624039ffdf2900c7d59f4f2
Junya-Izumi <xuanyequan@gmail.com>
2026-04-01 08:47:28 +0900

    fix:viewer/idnex.tsをviewer/index.tsに修正
    他ファイルのimport文も修正

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 //ツールの実行
 
 import { Solver } from "./solver";
-import { sampleData2} from "./data/sample";
-import { Viewer } from "./viewer/idnex";
-import { qs } from "./utiles";
+import { Viewer } from "./viewer";
+import { sampleData2 } from "./data/sample";
+import { qs } from "./utiles";//document.querySelector()の短縮
+
 const solver = new Solver(sampleData2)
-const viewer = new Viewer(solver,qs("#viewer") as HTMLElement)
-const solveBtn = qs("#solve") as HTMLElement;
-solveBtn.addEventListener("click",solver.solve.bind(solver))
+const viewer = new Viewer(solver,qs("#viewer")!)
+const solveBtn = qs("#solve")!
+solveBtn.addEventListener("click",()=>solver.solve())
--- src/utiles/index.ts
+++ src/utiles/index.ts
@@ -1,2 +1,2 @@
-export const qs = (selector: string, parent:Document|HTMLElement = document): Element | null => parent.querySelector<HTMLElement>(selector);
+export const qs = (selector: string, parent:Document|HTMLElement = document): HTMLElement | null => parent.querySelector<HTMLElement>(selector);
 export const qsAll = (selector: string, parent:Document|HTMLElement = document): NodeListOf<HTMLElement> | null => parent.querySelectorAll<HTMLElement>(selector);
